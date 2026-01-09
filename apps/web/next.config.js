851917072e6f7e9b417939060178154d7e7fa4b4
Krzysztof Dorzak <56868520+kdorzak@users.noreply.github.com>
2026-01-09 01:07:52 +0100

    revert webpack

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,15 +5,6 @@
  **/
 const nextConfig = {
   output: "export",
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...(config.resolve.fallback || {}),
-      fs: false,
-      path: false,
-      os: false,
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
