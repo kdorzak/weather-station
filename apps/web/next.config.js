a06e1cb46babb73a41dd917958881a2ca304b575
Krzysztof Dorzak <56868520+kdorzak@users.noreply.github.com>
2026-01-09 01:07:18 +0100

    fix build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,6 +5,15 @@
  **/
 const nextConfig = {
   output: "export",
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...(config.resolve.fallback || {}),
+      fs: false,
+      path: false,
+      os: false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
